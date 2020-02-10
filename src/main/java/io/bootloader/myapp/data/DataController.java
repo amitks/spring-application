230306100051ee10b8ea7c85602b8a384ef991d0
amit kumar <msnamit@gmail.com>
2020-02-11 01:22:44 +0530

    data api returns json

--- src/main/java/io/bootloader/myapp/data/DataController.java
+++ src/main/java/io/bootloader/myapp/data/DataController.java
@@ -1,5 +1,8 @@
 package io.bootloader.myapp.data;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class DataController {
 	
 	@RequestMapping("/data")
-	public String data_function() {
-		return "some random data!";
+	public List<dataModule> data_function() {
+
+		return Arrays.asList(
+				new dataModule("100", "john", "manager"),
+				new dataModule("101", "sam", "director"),
+				new dataModule("102", "matt", "engineer")
+				);
+
+		//return "some random data!";
 	}
 
 }
